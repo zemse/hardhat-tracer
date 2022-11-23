fa200b6c085345cc1a40256d782665eea2a6e839
Soham Zemse <22412996+zemse@users.noreply.github.com>
2022-11-23 15:09:27 +0000

    Add support for tracecall

---
+++ src/tasks/tracecall.ts
@@ -0,0 +1,90 @@
+import { addCliParams, applyCliArgsToTracer } from "../utils";
+import { HttpNetworkUserConfig } from "hardhat/types";
+import { task } from "hardhat/config";
+import { TraceRecorder } from "../trace/recorder";
+import { ethers } from "ethers";
+
+addCliParams(task("tracecall", "Traces a call"))
+  .addParam("to", "destination address")
+  .addOptionalParam("data", "input call data")
+  .addOptionalParam("value", "value to send")
+  .addOptionalParam("gasLimit", "gas limit")
+  .addOptionalParam("gasPrice", "gas price")
+  .addOptionalParam("blocknumber", "block number")
+  .addOptionalParam("rpc", "archive node")
+  .setAction(async (args, hre, runSuper) => {
+    applyCliArgsToTracer(args, hre);
+
+    // try using current mainnet fork url as rpc url
+    const mainnetForkUrl = (hre.network.config as any).forking?.url;
+    if (mainnetForkUrl) {
+      args.rpc = mainnetForkUrl;
+    }
+
+    // try using url specified in network as rpc url
+    if (args.network) {
+      const userNetworks = hre.userConfig.networks;
+      if (userNetworks === undefined) {
+        throw new Error("No networks found in hardhat config");
+      }
+      if (userNetworks[args.network] === undefined) {
+        throw new Error(`Network ${args.network} not found in hardhat config`);
+      }
+      const url = (userNetworks[args.network] as HttpNetworkUserConfig).url;
+      if (url === undefined) {
+        throw new Error(
+          `Url not found in hardhat-config->networks->${args.network}`
+        );
+      }
+      args.rpc = url;
+    }
+
+    if (!args.rpc) {
+      // TODO add auto-detect network
+      throw new Error(
+        "rpc url not provided, please either use --network <network-name> or --rpc <rpc-url>"
+      );
+    }
+
+    const provider = new ethers.providers.StaticJsonRpcProvider(args.rpc);
+    if (args.blocknumber === undefined) {
+      args.blocknumber = await provider.getBlockNumber();
+    }
+
+    console.warn("Activating mainnet fork at block", args.blocknumber);
+    await hre.network.provider.send("hardhat_reset", [
+      {
+        forking: {
+          jsonRpcUrl: args.rpc,
+          blockNumber: Number(args.blocknumber),
+        },
+      },
+    ]);
+
+    try {
+      const result = await hre.ethers.provider.call({
+        to: args.to,
+        data: args.data,
+        value: args.value,
+        gasLimit: args.gasLimit,
+        gasPrice: args.gasPrice,
+      });
+      console.log("result:", result);
+    } catch {}
+    // TODO try to do this properly
+    // @ts-ignore
+    const recorder = (global?._hardhat_tracer_recorder as unknown) as TraceRecorder;
+
+    await recorder.previousTraces[recorder.previousTraces.length - 1].print({
+      artifacts: hre.artifacts,
+      tracerEnv: hre.tracer,
+      provider: hre.ethers.provider,
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return;
+
+    // const txFromRpc = await provider.getTransaction(args.hash);
+
+    // after the above hardhat reset, tx should be present on the local node
+  });
--- test/project.test.ts
+++ test/project.test.ts
@@ -32,7 +32,7 @@ describe("Hardhat Runtime Environment extension", function () {
       assert.strictEqual(this.hre.tracer.enabled, true);
     });
 
-    it.only("mainnet by rpc", async function () {
+    it("mainnet by rpc", async function () {
       await this.hre.run("compile");
       await this.hre.run("trace", {
         hash:
@@ -57,6 +57,55 @@ describe("Hardhat Runtime Environment extension", function () {
 
     it.skip("arbitrum", async function () {
       await this.hre.run("trace", {
+        hash:
+          "0x64e36ed3441bae0ed9b1ce685cb14791806a857038a094753943007df3d74bc5",
+        rpc: "https://arb-mainnet.g.alchemy.com/v2/AS_LAx2_WJh1iEAqxd1-AHQ-yb71CiCHF".replace(
+          "_",
+          ""
+        ),
+      });
+    });
+  });
+
+  describe("Trace call", function () {
+    useEnvironment("hardhat-project");
+
+    before(async function () {
+      // await this.hre.run("compile");
+    });
+
+    it("Should be enabled when specified in config", function () {
+      assert.strictEqual(this.hre.tracer.enabled, true);
+    });
+
+    it.only("mainnet by rpc", async function () {
+      await this.hre.run("compile");
+      await this.hre.run("tracecall", {
+        to: "0xe592427a0aece92de3edee1f18e0157c05861564",
+        data:
+          "0x414bf389000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb4800000000000000000000000000000000000000000000000000000000000001f40000000000000000000000000f4ee9631f4be0a63756515141281a3e2b293bbe00000000000000000000000000000000000000000000000000000000627e90ee000000000000000000000000000000000000000000000003244c2a6bb0dc44220000000000000000000000000000000000000000000000000000001be27dca0d0000000000000000000000000000000000000000000000000000000000000000",
+        network: "mainnet",
+        blocknumber: "14768585",
+        // rpc: "https://eth-mainnet.alchemyapi.io/v2/AS_LAx2_WJh1iEAqxd1-AHQ-yb71CiCHF".replace(
+        //   "_",
+        //   ""
+        // ),
+        // v: true,
+        // opcodes: ["SSTORE", "SLOAD"].join(","),
+      });
+    });
+
+    it("mainnet by network", async function () {
+      await this.hre.run("compile");
+      await this.hre.run("tracecall", {
+        hash:
+          "0xc7f743c1bcd7fddfd6b644f6e5a3a97bdf5a02dfdff180a79f79f7c7481a5b0f",
+        network: "mainnet",
+      });
+    });
+
+    it.skip("arbitrum", async function () {
+      await this.hre.run("tracecall", {
         hash:
           "0x64e36ed3441bae0ed9b1ce685cb14791806a857038a094753943007df3d74bc5",
         rpc: "https://arb-mainnet.g.alchemy.com/v2/AS_LAx2_WJh1iEAqxd1-AHQ-yb71CiCHF".replace(
