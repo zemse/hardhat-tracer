05677aae3400339f6dcb4fab60841c95ed8abd0f
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-08-07 10:39:13 +0530

    Fix lint

--- src/opcodes/index.ts
+++ src/opcodes/index.ts
@@ -15,8 +15,8 @@ import log1 from "./log1";
 import log2 from "./log2";
 import log3 from "./log3";
 import log4 from "./log4";
-import msize from "./msize";
 import mload from "./mload";
+import msize from "./msize";
 import mstore from "./mstore";
 import mstore8 from "./mstore8";
 import revert from "./revert";
@@ -44,10 +44,10 @@ export function parse(
       return log3.parse(step, currentAddress);
     case "LOG4":
       return log4.parse(step, currentAddress);
-    case "MSIZE":
-      return msize.parse();
     case "MLOAD":
       return mload.parse(step);
+    case "MSIZE":
+      return msize.parse();
     case "MSTORE":
       return mstore.parse(step);
     case "MSTORE8":
@@ -88,10 +88,10 @@ export async function format(
     case "LOG3":
     case "LOG4":
       return log.format(item, dependencies);
-    case "MSIZE":
-      return msize.format(item);
     case "MLOAD":
       return mload.format(item);
+    case "MSIZE":
+      return msize.format(item);
     case "MSTORE":
       return mstore.format(item);
     case "MSTORE8":
